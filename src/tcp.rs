27c25fbfd9a880e2aad71c10ccdc2e095d4e450b
Mihai Galos <mihaigalos@gmail.com>
2023-04-07 16:27:41 +0200

    Stats: Initial implementation

--- src/tcp.rs
+++ src/tcp.rs
@@ -1,9 +1,15 @@
-use std::io;
 use std::net::{TcpListener, TcpStream};
 use std::sync::Arc;
 use std::thread;
 
 use std::time::SystemTime;
+use std::sync::mpsc;
+use std::sync::mpsc::Sender;
+use std::sync::mpsc::Receiver;
+use std::io::Read;
+use std::io::Write;
+
+
 
 pub fn route(from: &str, to: &str) -> std::io::Result<()> {
 
@@ -47,12 +53,14 @@ fn connection_handler(from: String, to: String, from_stream: TcpStream, to_strea
     let from_arc = Arc::new(from_stream);
     let to_arc = Arc::new(to_stream);
 
-    let (mut from_tx, mut from_rx) = (from_arc.try_clone().unwrap(), from_arc.try_clone().unwrap());
-    let (mut to_tx, mut to_rx) = (to_arc.try_clone().unwrap(), to_arc.try_clone().unwrap());
+    let from_tx = from_arc.try_clone().unwrap();
+    let to_tx = to_arc.try_clone().unwrap();
 
+    let (stats_tx, _) = mpsc::channel();
+    let (write_tx, write_rx) = mpsc::channel();
     let connections = vec![
-        thread::spawn(move || io::copy(&mut from_tx, &mut to_rx).unwrap()),
-        thread::spawn(move || io::copy(&mut to_tx, &mut from_rx).unwrap()),
+        thread::spawn(move || read_loop(from_tx, stats_tx, write_tx).unwrap()),
+        thread::spawn(move || write_loop(to_tx, write_rx).unwrap()),
     ];
 
 
@@ -66,4 +74,54 @@ fn connection_handler(from: String, to: String, from_stream: TcpStream, to_strea
         let timestamp = get_timestamp();
         println!("🔌 {timestamp} DISCONNECTED {from} -> {to}");
     }
+}
+
+pub fn write_loop(mut to_stream: TcpStream, write_rx: Receiver<Vec<u8>>) -> std::io::Result<()> {
+    loop {
+        // TODO receive bytes
+        let buffer = write_rx.recv().unwrap();
+
+        if buffer.is_empty() {
+            break;
+        }
+
+        if let Err(e) = to_stream.write_all(&buffer) {
+            if e.kind() == std::io::ErrorKind::BrokenPipe {
+                // Stop processing
+                return Ok(());
+            }
+
+            return Err(e);
+            // eprintln!("Error: {}", e.to_string());
+            // std::process::exit(1);
+        }
+    }
+    Ok(())
+}
+
+pub fn read_loop(
+    mut from_stream: TcpStream,
+    stats_tx: Sender<usize>,
+    write_tx: Sender<Vec<u8>>,
+) -> std::io::Result<()> {
+    let mut buffer = [0; 1024];
+
+    loop {
+        let num_read = match from_stream.read(&mut buffer) {
+            Ok(0) => break,
+            Ok(x) => x,
+            Err(_) => break,
+        };
+
+        let _ = stats_tx.send(num_read);
+
+        if write_tx.send(Vec::from(&buffer[..num_read])).is_err() {
+            break;
+        }
+    }
+
+    let _ = stats_tx.send(0);
+    let _ = write_tx.send(Vec::new());
+
+    Ok(())
 }
